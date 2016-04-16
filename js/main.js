00c02a4c694c4623bd850b5a5ca57d7b0e51959b
Anthony Zhang <azhang102@gmail.com>
2016-04-16 19:04:47 -0400

    made smooth transitions from list to post and post to list using ajax and history js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var cssBezier = new Ease(BezierEasing(.7,0,.3,1));
+var polaroidAngle = "-2deg";
 
 var site = {
 	breakpts : {
@@ -119,25 +120,104 @@ var site = {
 
     History.Adapter.bind(window, 'statechange', function(){
       var State = History.getState();
-      console.log(State.url);
       $.get(State.url, function(data){
-        document.title = $(data).find("title").text();
-        console.log($(data).find(".content"));
-        $('.content').html($(data).find('.content'));
+      	
+      	// this may fail at some point... can't grab it with "title" for some reason.
+        document.title = $($(data)[7]).text();	  	
+        
+        $(".swap-content.appear").removeClass("appear");
+        setTimeout(function(){
+        	$('.swap-content').html($(data).find('.swap-content').html());
+        	
+        	if ($(".swap-content.fixed-wrapper").children(".scrollable-contents"))
+          	$(".swap-content.fixed-wrapper").addClass("scrollable-wrapper").removeClass("fixed-wrapper");
+	  			if ($(".swap-content.scrollable-wrapper").children(".fixed-contents"))
+          	$(".swap-content.scrollable-wrapper").addClass("fixed-wrapper").removeClass("scrollable-wrapper");
+        	
+        	
+        	// if the url is going to a post, we can transition
+			  	var urlStr = State.url;
+			  	if (urlStr.indexOf("/posts") !== -1){
+			  		var urlStrSlug = urlStr.substr(urlStr.indexOf("/posts"));
+			  		var link = $("a[href='" + urlStrSlug + "']");
+			  		if (link) {
+			  			site.transitionToPost(link, $(data));
+			  		}
+			  	}
+			  	// default transition
+			  	else {
+			  		site.transitionDefault($(data));
+			  	}
+        	
+        	
+        	$('.swap-content').addClass("appear");
+        }, 300);
         // _gaq.push(['_trackPageview', State.url]);
       });
     });
+  },
+  transitionDefault : function($data) {
+  	TweenLite.to($(".polaroid-wrap"), 0.5, {
+  		y : 500,
+  		alpha : 0,
+  		ease : cssBezier,
+  		onComplete : function(){
+  			$(".page-name").attr("class", $data.find(".page-name").attr("class") );
+				$(".scrollable-fixed-wrap").append($data.find(".post-list-wrapper"));
+  			$(".post-list-wrapper").css("opacity", "0").css("transform", "translateY(500px)");
+  			setTimeout(function(){
+  				$(".polaroid-wrap.fixed-wrapper").remove();
+  				
+  				TweenLite.to($(".post-list-wrapper"), 0.5, {
+			  		y : 0,
+			  		alpha : 1,
+			  	});
+  			}, 100);
+  		}
+  	});
+  },
+  transitionToPost : function($polaroidLink, $data) {
+  	var polaroidNum = $polaroidLink.index() + 1;
+  	var distFromWindowTop = $polaroidLink.offset().top - $(window).scrollTop();
+  	var windowHeight = $(window).height();
+  	var polaroidHeight = $polaroidLink.find("li").outerHeight();
+  	
+  	// the ones we didn't click
+  	TweenLite.to($(".post-list a:not(:nth-child(" + polaroidNum + "))"), 0.5, {
+  		alpha : 0,
+  		x : -400,
+  		ease : cssBezier,
+  	});
+  	
+  	$(".post-list a:nth-child(" + polaroidNum + ") li").css("transform", "rotate(-2deg)");
+  	TweenLite.to($(".post-list a:nth-child(" + polaroidNum + ")"), 0.5, {
+  		y : ((windowHeight - polaroidHeight)/2) - distFromWindowTop,
+  		ease : cssBezier,
+  		onComplete : function(){
+  			$(".page-name").attr("class", $data.find(".page-name").attr("class") );
+  			$(".post-list-wrapper").addClass("lock");
+				$(".scrollable-fixed-wrap").append($data.find(".fixed-wrapper"));
+  			setTimeout(function(){
+  				$(".post-list-wrapper").remove();
+  			}, 100);
+  		}
+  	});
   }
 };
 
 
 
 $(document).ready(function(){
+	$(".swap-content").addClass("appear");
 	site.setUpHistoryJS();
 	
 	$(document).hammer().on("tap", ".modal .close", function(){
 		site.closeModal();
 	});
+	
+	// $(document).hammer().on("tap", ".post-list a", function(){
+	// 	site.transitionToPost($(this));
+	// });
 	
 	$(document).keydown(function(e){
 		// close modal.
